cb56dc23537bf4c34ecfe907e6d5a04c12a18456
Marco Pancotti <mpancotti@mate.it>
2014-11-22 16:27:49 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions do more or less the same actions that the example does, only using the
+## solve function instead of the mean function
 
-## Write a short comment describing this function
+## this function return a list of methods (get, setinverse and getinverse) that will be used 
+## by the cacheSolve function. 
+##
+## The getInverse function return the cachedInvertedMatrix if it has been already calculated. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cachedInvertedMatrix <- NULL
+    get <- function() {x}
+    setinverse <- function(invertedMatrix) cachedInvertedMatrix <<- invertedMatrix
+    getinverse <- function() cachedInvertedMatrix
+    list(get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function uses the methods created in the makeCacheMatrix function in order to use or set the 
+## cachedInvertedMatrix instead of calculate all the time. In ths way the solve function will be applied only the
+## first time you calculate the inverted value of a specific matrix. The subsequent times you need the same inverted
+## matrix the cached value will be used
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    invertedMatrix <- x$getinverse()
+    if(!is.null(invertedMatrix)) {
+      message("getting cached data")
+      return(invertedMatrix)
+    }
+    data <- x$get()
+    invertedMatrix <- solve(data, ...)
+    x$setinverse(invertedMatrix)
+    invertedMatrix
 }
